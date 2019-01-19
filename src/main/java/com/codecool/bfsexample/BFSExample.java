426464d19823397e4de333aa14777a1b6a663c33
Uj Levente <asdlevente@gmail.com>
2019-01-19 18:51:11 +0100

    get users in a distance feature implemented, breadth first search methods moved to BFSimpl class

--- src/main/java/com/codecool/bfsexample/BFSExample.java
+++ src/main/java/com/codecool/bfsexample/BFSExample.java
@@ -24,67 +24,10 @@ public class BFSExample {
         return users.get(ran.nextInt(users.size()));
     }
 
-    public static int getDistanceBetweenTwoUser(UserNode u1, UserNode u2) {
-
-        if (u1 == null || u2 == null)
-            throw new IllegalArgumentException("user not defined");
-
-        Queue<UserNode> toVisit = new LinkedList<>();
-        Set<UserNode> alreadyVisited = new HashSet<>();
-        toVisit.add(u1);
-        
-        final UserNode depthMarker = null;
-        int depthCounter = 0;
-        toVisit.add(depthMarker);
-
-        while (!toVisit.isEmpty()) {
-            UserNode current = toVisit.remove();
-
-            if (current == depthMarker) {
-                toVisit.add(depthMarker);
-                depthCounter++;
-                continue;
-            }
-
-            if (current.equals(u2))
-                return depthCounter;
-
-            alreadyVisited.add(current);
-
-            for (UserNode friend: current.getFriends()) {
-                if (!alreadyVisited.contains(friend)) {
-                    toVisit.add(friend);
-                }
-            }
-
-            current.getFriends().forEach(friend -> {
-                if (!alreadyVisited.contains(friend))
-                    toVisit.add(friend);
-            });
-
-        }
-
-        System.out.println("no path found.");
-        return -1;
-    }
-
-    public static List<UserNode> getFriendsInGivenDistance(int distance) {
-
-    }
-
-
     public static void main(String[] args) {
         populateDB();
-//        Queue<UserNode> toVisit = new LinkedList<>();
-//        toVisit.add(users.get(0));
-//        toVisit.add(users.get(1));
-//        toVisit.add(users.get(2));
-//        System.out.println(toVisit);
-//        System.out.println(toVisit.remove());
-//        toVisit.add(users.get(3));
-//        System.out.println(toVisit);
-//        System.out.println(toVisit.remove());
-//        System.out.println(getDistanceBetweenTwoUser(getRandomUser(), getRandomUser()));
-        //System.out.println(getDistanceBetweenTwoUser(users.get(0), users.get(3)));
+        BFSimpl bfs = new BFSimpl();
+        System.out.println(bfs.getDistanceBetweenTwoUser(users.get(0), users.get(2)));
+        System.out.println(bfs.getFriendsInGivenDepth(users.get(2), 1));
     }
 }
--- src/main/java/com/codecool/bfsexample/BFSimpl.java
+++ src/main/java/com/codecool/bfsexample/BFSimpl.java
@@ -1,4 +1,85 @@
 package com.codecool.bfsexample;
 
+import com.codecool.bfsexample.model.UserNode;
+
+import java.util.*;
+
 public class BFSimpl {
+
+    private Queue<UserNode> toVisit;
+    private Set<UserNode> alreadyVisited;
+    private final UserNode depthMarker = null;
+    private int depthCounter;
+
+    private void BFSinit(UserNode sourceNode) {
+        toVisit = new LinkedList<>();
+        alreadyVisited = new HashSet<>();
+        depthCounter = 0;
+        toVisit.add(sourceNode);
+    }
+
+    private void fillQueueWithAdjacentNodes(UserNode userNode) {
+        userNode.getFriends().forEach(friend -> {
+            if (!alreadyVisited.contains(friend))
+                toVisit.add(friend);
+        });
+    }
+
+    public int getDistanceBetweenTwoUser(UserNode u1, UserNode u2) {
+
+        if (u1 == null || u2 == null) throw new IllegalArgumentException("user not defined");
+
+        BFSinit(u1);
+        toVisit.add(depthMarker);
+
+        while (!toVisit.isEmpty()) {
+            UserNode current = toVisit.remove();
+
+            if (current == depthMarker) {
+                toVisit.add(depthMarker);
+                depthCounter++;
+                continue;
+            }
+
+            if (current.equals(u2))
+                return depthCounter;
+
+            alreadyVisited.add(current);
+            fillQueueWithAdjacentNodes(current);
+        }
+
+        System.out.println("no path found.");
+        return -1;
+    }
+
+    public Set<UserNode> getFriendsInGivenDepth(UserNode sourceUser, int depth) {
+        if (sourceUser == null) throw new IllegalArgumentException("source node not defined");
+        if (depth < 0) throw new IllegalArgumentException("depth should not be negative");
+
+        BFSinit(sourceUser);
+        toVisit.add(depthMarker);
+
+        while (!toVisit.isEmpty()) {
+            UserNode current = toVisit.remove();
+
+            if (current == depthMarker) {
+
+                if (depthCounter == depth) {
+                    alreadyVisited.remove(sourceUser);
+                    return alreadyVisited;
+                }
+
+                toVisit.add(depthMarker);
+                depthCounter++;
+                continue;
+            }
+
+            alreadyVisited.add(current);
+            fillQueueWithAdjacentNodes(current);
+        }
+
+        System.out.println("no friends found");
+        return new HashSet<>();
+    }
+
 }
